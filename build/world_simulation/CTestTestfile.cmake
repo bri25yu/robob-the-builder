97b52fccb3febb00ae6d5fc53c1c5d83e4f10beb
Bhavna Sud <bhavnasud@berkeley.edu>
2020-11-04 13:20:25 -0800

    initializing world simulation package

---
+++ build/world_simulation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bhavnasud/ros_workspaces/robob-the-builder/src/world_simulation
+# Build directory: /home/bhavnasud/ros_workspaces/robob-the-builder/build/world_simulation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
