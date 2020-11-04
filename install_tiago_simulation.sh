e2a1435a2de48dd96ae545d1ca3d43b3bebfb4e1
Brian Yu <bri25yu@gmail.com>
2020-11-04 14:24:46 -0800

    fixing install_tiago_simulation script to use catkin build from catkin build tools instead of catkin make

--- install_tiago_simulation.sh
+++ install_tiago_simulation.sh
@@ -12,8 +12,11 @@ rosdep update
 
 rosdep install --from-paths src --ignore-src --rosdistro kinetic --skip-keys="opencv2 opencv2-nonfree pal_laser_filters speed_limit_node sensor_to_cloud hokuyo_node libdw-dev python-graphitesend-pip python-statsd pal_filters pal_vo_server pal_usb_utils pal_pcl pal_pcl_points_throttle_and_filter pal_karto pal_local_joint_control camera_calibration_files pal_startup_msgs pal-orbbec-openni2 dummy_actuators_manager pal_local_planner gravity_compensation_controller current_limit_controller dynamic_footprint dynamixel_cpp tf_lookup slam_toolbox"
 
+sudo apt-get update
+sudo apt-get install python-catkin-tools
+
 source /opt/ros/kinetic/setup.bash
-catkin_make
+catkin build -DCATKIN_ENABLE_TESTING=0
 
 source ./devel/setup.bash
 roslaunch tiago_gazebo tiago_gazebo.launch public_sim:=true robot:=steel
