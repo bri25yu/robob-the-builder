6fd01af4b22a9944cf632d929d2b20898cbb84b8
Bhavna Sud <bhavnasud@berkeley.edu>
2020-11-04 21:43:39 -0800

    removing untracked files

--- build/world_simulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bhavnasud/ros_workspaces/robob-the-builder/src/world_simulation
-# Build directory: /home/bhavnasud/ros_workspaces/robob-the-builder/build/world_simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
