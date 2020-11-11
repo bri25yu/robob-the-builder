851b28594b81b6d38de64ecc743a6ff13a35b6c7
Brian Yu <bri25yu@gmail.com>
2020-11-11 12:58:52 -0800

    adding links for model work done today

--- init_models.sh
+++ init_models.sh
@@ -1,2 +1,6 @@
+# Camera files from http://models.gazebosim.org/camera/
+# SDF format reference http://sdformat.org/spec?ver=1.7&elem=sensor
+# Maybe useful for how to use SDF files? http://gazebosim.org/tutorials?tut=ros_roslaunch
+
 rm -rf ~/.gazebo/models/camera
 cp -R ./src/world_simulation/models/camera ~/.gazebo/models
