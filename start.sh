16e3251aa088528f17a8dd67d0e154712a6f5701
Brian Yu <bri25yu@gmail.com>
2020-11-30 15:57:12 -0800

    adding full script to start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,26 @@ then
 
     catkin_make
     source devel/setup.bash
+
     chmod +x src/world_simulation/src/add_block.py
+    chmod +x src/world_simulation/src/take_photos.py
+    chmod +x src/generate_schematic/src/generate_schematic.py
+
+    chmod +x stop_gazebo.sh
 fi
 source devel/setup.bash
+
+# Start necessary services: roscore and Gazebo
+roscore &
+rosrun gazebo_ros gazebo &
+
+# Create world. Make any world modifications in src/global_constants/src/constants.py
 rosrun world_simulation add_block.py
+rosrun world_simulation take_photos.py
+
+# Generate the schematic to build
+rosrun generate_schematic generate_schematic.py
+
+# Shut down services
+./stop_gazebo.sh
+killall -9 roscore & killall -9 rosmaster
