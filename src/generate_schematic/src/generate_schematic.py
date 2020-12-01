596dd41ac0eb1902cc2d2177d5886ff6bb3a2685
Brian Yu <bri25yu@gmail.com>
2020-11-30 16:01:39 -0800

    refactoring generate_schematic

--- src/generate_schematic/src/generate_schematic.py
+++ src/generate_schematic/src/generate_schematic.py
@@ -3,23 +3,24 @@
 import rospy
 import cv2
 
+from feature_detect import FeatureDetect
+from camera import CameraDTO
+
 
 IMAGE_IN_PATH = "images/gazebo_angled_image_one.jpg"
 IMAGE_OUT_NAME = "gazebo_angled_image_one_clustering.jpg"
 
+OUTPUT_FILE = "output/schematic.txt"
+
 def main():
     rospy.init_node("schematic_node", anonymous = True)
-    g = GenerateSchematic()
-    coordinates = g.match_images([2, 3])
-    # img = g.get_image(IMAGE_IN_PATH)
-    # bottom_left_coordinates = g.find_all_bottom_left_coordinates_2d(img)
-    #
-    # #display an image with bottom left coordinates highlighted in white
-    # for coordinates in bottom_left_coordinates:
-    #     cv2.circle(img, coordinates, 3, (255, 255, 255), -1)
-    # cv2.imshow("BottomLeftCoordinates", img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+
+    camera2, camera3 = CameraDTO(2), CameraDTO(3)
+    coordinates = FeatureDetect.find_all_corners_3d(camera2, camera3)
+
+    with open(OUTPUT_FILE, "w") as file:
+        for coord in coordinates:
+            file.write("{}".format(coord))
 
 
 if __name__ == "__main__":
